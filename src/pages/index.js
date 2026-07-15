17961fdcf229cf6db15864f08d786d57dbedd787
jatinagrahari <strangerguy.2311@gmail.com>
2026-07-16 00:00:47 +0530

    files and the folder structure created

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from "./Home/Home";
+import About from "./About/About";
+import Contact from "./Contact/Contact";
+import Experience from "./Experience/Experience";
+import Projects from "./Projects/Projects";
+
+export { Home, About, Contact, Experience, Projects };
