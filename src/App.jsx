203e7dba9be75575bebec8e8fbfd752df37acc3d
jatinagrahari <strangerguy.2311@gmail.com>
2026-07-16 00:17:20 +0530

    files and the folder structure created

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,18 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
+import Layout from "./components/Layout/Layout";
+import { Home, About, Contact, Experience, Projects } from "./pages";
+
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element="">
-        <Route />
+      <Route path="/" element={<Layout />}>
+        <Route path="" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/Experience" element={<Experience />} />
+        <Route path="/contact" element={<Contact />} />
       </Route>,
     ),
   );
